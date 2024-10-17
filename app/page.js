8172ae2590a3aacb2788712ee92b44b4ec61b829
shlayb <ayubisal1345@gmail.com>
2024-10-17 15:22:34 +0700

    Initial commit

--- app/page.js
+++ app/page.js
@@ -5,6 +5,7 @@ import { getAnalytics } from 'firebase/analytics';
 import { getDatabase, ref, set, onValue } from 'firebase/database';
 import { use, useState, useEffect, useCallback } from 'react';
 import { Poppins } from 'next/font/google';
+import ToggleAuto from '@/components/toggleauto';
 const poppins = Poppins({
   subsets: ['latin'],
   weight: ['400', '700'],
@@ -43,7 +44,7 @@ export default function Home() {
     set(ref(db, 'servo'), servo === 0 ? 1 : 0);
   };
   return (
-    <div>
+    <div className="h-lvh">
       <div className="w-full h-auto grid gap-2 justify-items-center p-12 mt-10 max-lg:gap-1 max-lg:mt-28">
         <h1 className="font-Poppins font-bold text-xl max-lg:text-lg ">ELINS PLANET PROJECT</h1>
         <h1 className="font-Poppins font-bold text-5xl max-lg:text-3xl">APIN : Atap Pintar</h1>
@@ -54,7 +55,7 @@ export default function Home() {
       <div className="w-full flex justify-center">
         <button
           className={`${
-            servo === 0 ? 'bg-red-600 border-dark border-4' : 'bg-slate-400 border-slate-600 border-4'
+            servo === 0 ? 'bg-red-500 border-dark border-4' : 'bg-slate-400 border-slate-600 border-4'
           } min-w-32 font-Poppins dark:shadow-slate-700 font-bold  text-light aspect-square rounded-full shadow-slate-600 shadow-lg hover:shadow-xl hover:shadow-slate-300 hover:scale-105 hover:duration-300 transform transition-all ease-in-out duration-300 active:scale-95`}
           onClick={handleServo}
         >
@@ -62,6 +63,10 @@ export default function Home() {
             <i class="fa-solid fa-power-off"></i>
           </p>
         </button>
+      </div>
+      {/* Toggle Auto */}
+      <div className="w-full flex justify-center">
+        <ToggleAuto />
       </div>
     </div>
   );
---
+++ components/toggleauto.js
@@ -0,0 +1,52 @@
+'use client';
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+import { getDatabase, ref, set, onValue } from 'firebase/database';
+import { use, useState, useEffect, useCallback } from 'react';
+
+export default function ToggleAuto() {
+  const [modeAuto, setAuto] = useState(null);
+
+  const firebaseConfig = {
+    apiKey: 'AIzaSyAVAgUbn0L5J8LXeQP9C2-CijAqy-51Cp0',
+    authDomain: 'projectep-abbca.firebaseapp.com',
+    databaseURL: 'https://projectep-abbca-default-rtdb.asia-southeast1.firebasedatabase.app',
+    projectId: 'projectep-abbca',
+    storageBucket: 'projectep-abbca.appspot.com',
+    messagingSenderId: '647038433468',
+    appId: '1:647038433468:web:519557d34f7bc92cda38b9',
+    measurementId: 'G-KRNY6MCYBD',
+  };
+  const app = initializeApp(firebaseConfig);
+  const db = getDatabase(app);
+  const readData = useCallback(() => {
+    const servoRef = ref(db, 'isAuto');
+    onValue(servoRef, (snapshot) => {
+      const data = snapshot.val();
+      setAuto(data);
+    });
+  }, [db]);
+
+  useEffect(() => {
+    readData();
+  }, [readData]);
+
+  const handleAuto = () => {
+    setAuto(modeAuto === 0 ? 1 : 0);
+    set(ref(db, 'isAuto'), modeAuto === 0 ? 1 : 0);
+  };
+
+  return (
+    <div className="p-8">
+      <button
+        className={`${modeAuto == 0 ? 'bg-slate-400 justify-start duration-300' : 'bg-red-500 justify-end duration-300'}
+          shadow-slate-400 shadow-lg min-w-32 min-h-14 rounded-full bg-slate-400 transition-all duration-300 flex items-center`}
+        onClick={handleAuto}
+      >
+        {modeAuto == 1 ? <span className="font-Poppins font-medium text-xl p-3 text-gray-200">On</span> : <span></span>}
+        <div className={`${modeAuto == 1 ? 'shadow shadow-slate-200' : 'shadow shadow-slate-500'} transition-all duration-300 w-14 h-14 bg-white rounded-full shadow-md m-1`}></div>
+        {modeAuto == 0 ? <span className="inline-block align-middle font-Poppins font-medium text-xl p-3 text-gray-600">Off</span> : <span></span>}
+      </button>
+    </div>
+  );
+}
