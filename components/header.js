9f53fe1d1683f410d0a9689aa61930dd5ed2de06
shlayb <ayubisal1345@gmail.com>
2024-10-18 12:33:33 +0700

    Initial commit

--- components/header.js
+++ components/header.js
@@ -63,7 +63,7 @@ function Header() {
                     setMobileNav(!Mobilenav);
                   }}
                 >
-                  <svg className="h-7 w-7 text-gray-80" x-show="!showMenu" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
+                  <svg className="h-7 w-7 text-gray-80" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" stroke="currentColor">
                     <path d="M4 6h16M4 12h16M4 18h16"></path>
                   </svg>
                 </button>
